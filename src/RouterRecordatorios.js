43bd4e051286630fcb0ec3953bdafd90ad7e15ef
eerjoker <rivera.esteban1994@gmail.com>
2021-06-13 12:54:57 -0300

    manejo de errores y cambio instanciacion del factory

--- src/RouterRecordatorios.js
+++ src/RouterRecordatorios.js
@@ -1,14 +1,30 @@
 import express from 'express'
 import crearFactoryCU from './FactoryEnviarRecordReservas.js'
 
+const factoryCU = crearFactoryCU()
+
 function crearRouterRecordatorios() {
   const router = express.Router()
-  const factoryCU = crearFactoryCU()
   
-  router.post('/', async (req, res) => {
-    const CU_EnviarRecordatorios = factoryCU.crearCU()
-    await CU_EnviarRecordatorios.ejecutar(req.body)
-    res.status(201).json({ message: 'mails enviados a los usuarios' })
+  router.post('/', async (req, res, next) => {
+    try {
+      const CU_EnviarRecordatorios = factoryCU.crearCU()
+      await CU_EnviarRecordatorios.ejecutar(req.body)
+      res.status(201).json({ message: 'Mails enviados a los usuarios' })
+    } catch (err) {
+      next(err)
+    }
+  })
+
+  router.use((err, req, res, next) => {
+    if(err.type === 'ERROR_DATOS_INVALIDOS') {
+      res.status(400)
+    } else if(err.type === 'ERROR_MAIL_NO_ENVIADO') {
+      res.status(500)
+    } else {
+      res.status(500)
+    }
+    res.json({ message: err.message })
   })
 
   return router
